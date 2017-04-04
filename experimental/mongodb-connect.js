b9b8c6d9cf4fe1bfb2edd296021914b0162284bb
jefedeltejas <jeff.jackson@dashiell.com>
2017-04-04 09:36:27 -0500

    printing a name object to the console

--- experimental/mongodb-connect.js
+++ experimental/mongodb-connect.js
@@ -1,5 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 
+var user = {name: 'pimpC', age: 34};
+var {name} = user;
+console.log(name);
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     console.log('Unable to connect to MongoDB server');
@@ -17,16 +21,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // });
 
   // Insert new doc into Users (name, age, location)
-  db.collection('Users').insertOne({
-    name: 'Chad Butler',
-    age: 34,
-    location: 'Port Arthur'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to add user', err);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Chad Butler',
+  //   age: 34,
+  //   location: 'Port Arthur'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to add user', err);
+  //   }
+  //   // console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   // var connect = db.collection('Users').find({});
   // console.log(connect);
