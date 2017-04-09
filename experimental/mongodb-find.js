734385d7770486c8c59a8506f21f083e7ce1d4a5
jefedeltejas <jeff.jackson@dashiell.com>
2017-04-08 22:10:14 -0500

    counts the todos now

--- experimental/mongodb-find.js
+++ experimental/mongodb-find.js
@@ -16,13 +16,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
 
 
-  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58e99e2637fae86baa92e924')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to get them todos fo ya main.', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
   }, (err) => {
     console.log('Unable to get them todos fo ya main.', err);
   });
-
 
   db.close();
 });
