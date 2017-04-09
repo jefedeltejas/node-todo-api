ec819fb0c86b373dafb40bd51b10f6d2077807a2
jefedeltejas <jeff.jackson@dashiell.com>
2017-04-08 21:31:06 -0500

    started new function code mongodb find

---
+++ experimental/mongodb-find.js
@@ -0,0 +1,25 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+// var user = {name: 'pimpC', age: 34};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to Mongo server');
+
+  
+
+  db.collection('Todos').find().count().then((count) => {
+
+  });
+
+
+  // db.close();
+});
