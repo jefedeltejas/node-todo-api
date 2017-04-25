74dd865b0b478251e57f08d6ff9aad2d1acec0a3
jefedeltejas <jeff.jackson@dashiell.com>
2017-04-25 05:20:23 -0500

    fleshed out scheme for mongoose

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -26,9 +31,7 @@ var Todo = mongoose.model('Todo', {
 // });
 //
 var otherTodo = new Todo({
-  text: 'Buy baby milk',
-  completed: true,
-  completedAt: 123
+  text: ' Load Video to MobileJukebox  '
 });
 
 otherTodo.save().then((doc) => {
