2d5a1a359605605e819963c0bcc1ff1184d12736
jefedeltejas <jeff.jackson@dashiell.com>
2017-04-25 03:35:10 -0500

    starting server component dir

--- experimental/findUserModifyObj.js
+++ experimental/findUserModifyObj.js
@@ -14,30 +14,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to Mongo server');
 
-db.collection('Users').findOneAndUpdate({
-  _id: new ObjectID('58fe02e0a2e66a51a4e9ffd7')
-}, {
-  $set: {
-    name: 'Phat Pat',
-    location: 'H-Town'
-  }
-}, {
-  returnOriginal: false
-}).then((result) => {
-  console.log(result);
-});
-
 // db.collection('Users').findOneAndUpdate({
-//   _id: new ObjectID('58fe017d0730b3510b43e628')
+//   _id: new ObjectID('58fe02e0a2e66a51a4e9ffd7')
 // }, {
-//   $inc: {
-//     age: 1
+//   $set: {
+//     name: 'Phat Pat',
+//     location: 'H-Town'
 //   }
 // }, {
 //   returnOriginal: false
 // }).then((result) => {
 //   console.log(result);
 // });
+
+db.collection('Users').findOneAndUpdate({
+  _id: new ObjectID('58fe02e0a2e66a51a4e9ffd7')
+}, {
+  $inc: {
+    age: 1
+  }
+}, {
+  returnOriginal: false
+}).then((result) => {
+  console.log(result);
+});
 
   //
   // // Insert new doc into Users (name, age, location)
