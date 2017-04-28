f5e063462eb12362b8058eb1072bc67826ca5987
jefedeltejas <jeff.jackson@dashiell.com>
2017-04-28 05:42:18 -0500

    completed server.test.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,6 +3,9 @@ const expect = require('expect'),
       {app} = require('./../server'),
       {Todo} = require('./../models/todo');
 
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -12,6 +15,19 @@ describe('POST /todos', () => {
       .post('/todos')
       .send({text})
       .expect(200)
-      
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
   });
 });
