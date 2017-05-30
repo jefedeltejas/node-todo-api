0f79b65a0b73bcaaebe7e52ea41e4025afc955c8
jefedeltejas <jeff.jackson@dashiell.com>
2017-05-30 12:10:17 -0500

    hash user psword befroe saving

--- experimental/hashing.js
+++ experimental/hashing.js
@@ -1,15 +1,33 @@
 const {SHA256} = require('crypto-js'),
-      jwt = require('jsonwebtoken');
+      jwt = require('jsonwebtoken'),
+      bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = '777leprechaun!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+// var hashedPassword = '$2a$10$g55Ze0ns79832Cp7LPdzUOlFxR4clnExEdsFRzDPUxWahTz/OlLd.';
+var hashedPassword = '$2a$10$bDd/OihiMZ.gjSNWFMjMG.gx.Hf4D39r2ZnMHF1Fx4UpIqEikfKRK';
+// interesting that both versions of hashedPassword return true value.
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // var message = 'I am user number 3',
